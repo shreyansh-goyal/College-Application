fb02110b1faa02d44dbf0fc41d235c5cd3d7c6c5
shreyanshgoyal90@gmail.com <shreyanshgoyal90@gmail.com>
2020-03-10 22:46:57 +0530

    change in the login page configuration update

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -11,7 +11,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import  {backendConnectivity} from "../../config/backendConnectivity"
+import  backendConnectivity from "../../config/backendConnectivity"
 export default class LoginPage extends Component{
     constructor(props)
     {
